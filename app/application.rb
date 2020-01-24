f1072a94ce705a130b8351b99b39f4346c7adf07
chic-page-6502 <zacharytq@gmail.com>
2020-01-24 01:13:35 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,6 +11,10 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+    elsif req.path.match(/cart/)
+      @@items.each do |item|
+        resp.write "#{item}"
+      end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
