57b75773759bc24755849d71ee0b9e97f5377a93
chic-page-6502 <zacharytq@gmail.com>
2020-01-24 01:30:21 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -29,7 +29,7 @@ class Application
         @@cart << search_term
         resp.write "added #{search_term}"
       else
-        resp.write "Item not found."
+        resp.write "We don't have that item."
       end
     else
       resp.write "Path Not Found"
