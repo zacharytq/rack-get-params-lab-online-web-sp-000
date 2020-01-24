82b365d1652bbe2c276d8e35a68645222fbf4df7
chic-page-6502 <zacharytq@gmail.com>
2020-01-24 01:29:25 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -27,6 +27,7 @@ class Application
 
       if @@items.include?(search_term)
         @@cart << search_term
+        resp.write "added #{search_term}"
       else
         resp.write "Item not found."
       end
